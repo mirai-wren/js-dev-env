f5b1a6d59003c5e5d14d545521feb6d1f424f2f6
Mirai Wren <mirai.wren@protonmail.com>
2018-06-16 22:54:24 +0100

    Add basic JSON mocking

--- build_scripts/server.js
+++ build_scripts/server.js
@@ -17,6 +17,14 @@ app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(request, response) {
+  response.json([
+    {"id": 1, "firstName":"Bob", "lastName":"Smith", "email":"bob@gmail.com"},
+    {"id": 2, "firstName":"Tammy", "lastName":"Norton", "email":"tnorton@yahoo.com"},
+    {"id": 3, "firstName":"Tina", "lastName":"Lee", "email":"lee.tina@hotmail.com"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if(err) {
     console.log(err);
